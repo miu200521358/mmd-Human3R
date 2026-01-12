63162817d801fa343e511377261c74c0ee300bf2
miu200521358 <garnet200521358@gmail.com>
2026-01-12 23:44:08 +0900

    CUDAアーキテクチャの設定を更新: compute_75のgencodeを有効化し、compute_86のgencodeを無効化

--- src/croco/models/curope/setup.py
+++ src/croco/models/curope/setup.py
@@ -10,9 +10,9 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 # alternatively, you can list cuda archs that you want, eg:
 all_cuda_archs = [
 # '-gencode', 'arch=compute_70,code=sm_70',
-# '-gencode', 'arch=compute_75,code=sm_75',
+'-gencode', 'arch=compute_75,code=sm_75',
 # '-gencode', 'arch=compute_80,code=sm_80',
-'-gencode', 'arch=compute_86,code=sm_86'
+# '-gencode', 'arch=compute_86,code=sm_86'
 ]
 
 setup(
